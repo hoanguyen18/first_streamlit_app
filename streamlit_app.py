a07e922430be3c116002b0f118ffab427bfa78a2
Hoa Nguyen <86810761+hoanguyen18@users.noreply.github.com>
2023-10-13 08:44:40 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit
 streamlit.title('My new healthy diner spot')
+
+streamlit.header('Breakfast Favorites')
+
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔Hard-Boiled Free-Range Egg')
